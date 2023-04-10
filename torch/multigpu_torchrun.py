d6c76bbf67dc15fc7463cbaa0034dc9952908996
Mathias <mathias.zinnen@gmail.com>
2023-04-10 13:12:48 +0200

    Split save and load paths

--- torch/multigpu_torchrun.py
+++ torch/multigpu_torchrun.py
@@ -13,6 +13,7 @@ from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
 import os
+from os.path import splitext
 
 
 def ddp_setup():
@@ -21,12 +22,13 @@ def ddp_setup():
 
 class Trainer:
     def __init__(
-        self,
-        model: torch.nn.Module,
-        train_data: DataLoader,
-        optimizer: torch.optim.Optimizer,
-        save_every: int,
-        snapshot_path: str,
+            self,
+            model: torch.nn.Module,
+            train_data: DataLoader,
+            optimizer: torch.optim.Optimizer,
+            save_every: int,
+            save_model_pth: str,
+            load_model_pth: str,
     ) -> None:
         self.gpu_id = int(os.environ["LOCAL_RANK"])
         self.model = model.to(self.gpu_id)
@@ -34,10 +36,10 @@ class Trainer:
         self.optimizer = optimizer
         self.save_every = save_every
         self.epochs_run = 0
-        self.snapshot_path = snapshot_path
-        if os.path.exists(snapshot_path):
-            print("Loading snapshot")
-            self._load_snapshot(snapshot_path)
+        self.save_model_pth = save_model_pth
+        if load_model_pth is not None:
+            print(f"Loading snapshot from {load_model_pth}")
+            self._load_snapshot(load_model_pth)
 
         self.model = DDP(self.model, device_ids=[self.gpu_id])
 
@@ -69,14 +71,16 @@ class Trainer:
             "MODEL_STATE": self.model.module.state_dict(),
             "EPOCHS_RUN": epoch,
         }
-        torch.save(snapshot, self.snapshot_path)
-        print(f"Epoch {epoch} | Training snapshot saved at {self.snapshot_path}")
+        save_pth = f'{splitext(self.save_model_pth)[0]}_{epoch}.pth'
+        torch.save(snapshot, save_pth)
+        print(f"Epoch {epoch} | Training snapshot saved at {save_pth}")
 
     def train(self, max_epochs: int):
-        for epoch in range(self.epochs_run, max_epochs):
+        for epoch in range(self.epochs_run + 1, max_epochs + 1):
             self._run_epoch(epoch)
             if self.gpu_id == 0 and epoch % self.save_every == 0:
                 self._save_snapshot(epoch)
+        self._save_snapshot(epoch)
 
 
 def load_train_objs(img_root, annFile):
@@ -97,24 +101,27 @@ def prepare_dataloader(dataset: Dataset, batch_size: int):
     )
 
 
-def main(save_every: int, total_epochs: int, batch_size: int, train_imgs, train_anns, snapshot_path):
+def main(save_every: int, total_epochs: int, batch_size: int, train_imgs, train_anns, output_model_pth, load_model_pth):
     ddp_setup()
     dataset, model, optimizer = load_train_objs(train_imgs, train_anns)
     train_data = prepare_dataloader(dataset, batch_size)
-    trainer = Trainer(model, train_data, optimizer, save_every, snapshot_path)
+    trainer = Trainer(model, train_data, optimizer, save_every, output_model_pth, load_model_pth)
     trainer.train(total_epochs)
     destroy_process_group()
 
 
 if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser(description='simple distributed training job')
     parser.add_argument('total_epochs', type=int, help='Total epochs to train the model')
     parser.add_argument('save_every', type=int, help='How often to save a snapshot')
     parser.add_argument('--batch_size', default=32, type=int, help='Input batch size on each device (default: 32)')
     parser.add_argument('--train_imgs', default=None, type=str, help='Path to folder containing training images')
     parser.add_argument('--train_anns', default=None, type=str, help='Path to training annotations file')
-    parser.add_argument('--output_model_pth', default='snapshot.pth' ,type=str, help='Where to save the trained model')
+    parser.add_argument('--output_model_pth', default='snapshot.pth', type=str, help='Where to save the trained model')
+    parser.add_argument('--load_model_pth', default=None, type=str, help='Path to checkpoint to continue training from')
     args = parser.parse_args()
-    
-    main(args.save_every, args.total_epochs, args.batch_size, args.train_imgs, args.train_anns, args.output_model_pth)
+
+    main(args.save_every, args.total_epochs, args.batch_size, args.train_imgs, args.train_anns,
+         args.output_model_pth, args.load_model_pth)
