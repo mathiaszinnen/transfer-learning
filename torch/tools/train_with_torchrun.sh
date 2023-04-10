dd4bb59619975100d141360175d4c3a0e4d1087a
Mathias <mathias.zinnen@gmail.com>
2023-04-10 13:14:04 +0200

    Split save and load paths

--- torch/tools/train_with_torchrun.sh
+++ torch/tools/train_with_torchrun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-torchrun --standalone ../multigpu_torchrun.py 10 10
+#torchrun --standalone ../multigpu_torchrun.py 10 10
 
+
+torchrun --standalone ../multigpu_torchrun.py 20 10 --load_model_pth 'snapshot_10.pth'
 
