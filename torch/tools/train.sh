6fff3712fbffa28e1b8bc347c0f39ad48e8345a8
Mathias Zinnen~ <mathias.zinnen@fau.de>
2023-04-12 15:20:10 +0200

    Update batch size in single gpu train script

--- torch/tools/train.sh
+++ torch/tools/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python ../multigpu_torchrun.py 10 10 --train_imgs /hdd/datasets/ODOR-v3/imgs --train_anns /hdd/datasets/ODOR-v3/instances_train.json
+python ../multigpu_torchrun.py 10 10 --train_imgs /hdd/datasets/ODOR-v3/imgs --train_anns /hdd/datasets/ODOR-v3/instances_train.json --batch_size 8
 
 
 #torchrun --standalone ../multigpu_torchrun.py 20 10 --load_model_pth 'snapshot_10.pth'
