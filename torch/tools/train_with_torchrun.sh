5b47707c7f19cc110d7e6e4ea0bb6d4a21c39a26
Mathias <mathias.zinnen@gmail.com>
2023-04-21 16:50:58 +0200

    WIP: Integrate validation step

--- torch/tools/train_with_torchrun.sh
+++ torch/tools/train_with_torchrun.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-torchrun --standalone --nproc_per_node=gpu ../multigpu_torchrun.py 10 10 --train_anns /hdd/datasets/ODOR-v3/instances_train.json --train_imgs /hdd/datasets/ODOR-v3/imgs --batch_size 8
+torchrun --standalone --nproc_per_node=gpu ../multigpu_torchrun.py 10 10 \
+  --train_anns /hdd/datasets/ODOR-v3/validation_quicksplit/instances_train.json \
+  --valid_anns /hdd/datasets/ODOR-v3/validation_quicksplit/instances_valid.json \
+  --train_imgs /hdd/datasets/ODOR-v3/imgs --batch_size 8
 
 
 #torchrun --standalone ../multigpu_torchrun.py 20 10 --load_model_pth 'snapshot_10.pth'
