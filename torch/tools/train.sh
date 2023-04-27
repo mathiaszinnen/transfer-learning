798e79bb1831ae4793094e1326391ea5be816f06
Mathias Zinnen~ <mathias.zinnen@fau.de>
2023-04-27 18:15:05 +0200

    Rename train script

--- torch/tools/train.sh
+++ torch/tools/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python ../multigpu_torchrun.py 10 10 --train_imgs /hdd/datasets/ODOR-v3/imgs --train_anns /hdd/datasets/ODOR-v3/validation_quicksplit/instances_train.json --valid_anns /hdd/datasets/ODOR-v3/validation_quicksplit/instances_valid.json --batch_size 8
+python ../train.py 10 10 --train_imgs /hdd/datasets/ODOR-v3/imgs --train_anns /hdd/datasets/ODOR-v3/validation_quicksplit/instances_train.json --valid_anns /hdd/datasets/ODOR-v3/validation_quicksplit/instances_valid.json --batch_size 8 --wandb
 
 
 #torchrun --standalone ../multigpu_torchrun.py 20 10 --load_model_pth 'snapshot_10.pth'
