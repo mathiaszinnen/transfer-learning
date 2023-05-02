83400bf021ee839833108d626d632a99b0c6545d
Mathias Zinnen~ <mathias.zinnen@fau.de>
2023-05-02 21:14:48 +0200

    add test shellscript

---
+++ plain_torch/tools/test.sh
@@ -0,0 +1,1 @@
+python ../test.py --load_model_path snapshot_10.pth --test_anns /hdd/datasets/annotations-nightly/snapshots/v3/instances_test.json --test_imgs /hdd/datasets/annotations-nightly/imgs --preds_pth 'preds.json'
