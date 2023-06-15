9cb60d9f8b7167e2de026f534707c2b1d252bf64
Mathias Zinnen~ <mathias.zinnen@fau.de>
2023-06-15 10:02:55 +0200

    Update test script

--- plain_torch/tools/test.sh
+++ plain_torch/tools/test.sh
@@ -1,1 +1,9 @@
-python ../test.py --load_model_path snapshot_10.pth --test_anns /hdd/datasets/annotations-nightly/snapshots/v3/instances_test.json --test_imgs /hdd/datasets/annotations-nightly/imgs --preds_pth 'preds.json'
+#python ../test.py --load_model_path snapshot_10.pth \
+#	--test_anns /hdd/datasets/annotations-nightly/snapshots/v3/instances_test.json \
+#	--test_imgs /hdd/datasets/annotations-nightly/imgs \
+#	--preds_pth 'preds.json'
+
+python ../test.py --load_model_path snapshot_10.pth \
+	--test_anns /hdd/datasets/annotations-nightly/snapshots/v3/validation_quicksplit/instances_train.json \
+	--test_imgs /hdd/datasets/annotations-nightly/imgs \
+	--preds_pth 'preds.json'
