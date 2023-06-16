c958fdec403930dd4342ce9bbd9d7b992f43640f
Mathias Zinnen~ <mathias.zinnen@fau.de>
2023-06-16 10:59:40 +0200

    Update lr and add preds

--- plain_torch/tools/test.sh
+++ plain_torch/tools/test.sh
@@ -3,7 +3,7 @@
 #	--test_imgs /hdd/datasets/annotations-nightly/imgs \
 #	--preds_pth 'preds.json'
 
-python ../test.py --load_model_path snapshot_10.pth \
+python ../test.py --load_model_path snapshot_30.pth \
 	--test_anns /hdd/datasets/annotations-nightly/snapshots/v3/validation_quicksplit/instances_train.json \
 	--test_imgs /hdd/datasets/annotations-nightly/imgs \
-	--preds_pth 'preds.json'
+	--preds_pth 'preds_30.json'
--- plain_torch/tools/train_with_torchrun.sh
+++ plain_torch/tools/train_with_torchrun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-torchrun --standalone --nproc_per_node=gpu ../train.py 10 10 \
+torchrun --standalone --nproc_per_node=gpu ../train.py 30 10 \
   --train_anns /hdd/datasets/ODOR-v3/validation_quicksplit/instances_train.json \
   --valid_anns /hdd/datasets/ODOR-v3/validation_quicksplit/instances_valid.json \
   --train_imgs /hdd/datasets/ODOR-v3/imgs --batch_size 8 \
